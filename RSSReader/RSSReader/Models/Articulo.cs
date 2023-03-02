9153344fd9f0d50c28955cf754b1a407f59955e2
javi alvarez <javieralvarezcu@gmail.com>
2023-03-02 10:22:27 +0100

    init project API

---
+++ RSSReader/RSSReader/Models/Articulo.cs
@@ -0,0 +1,10 @@
+﻿namespace RSSReaderApi.Models
+{
+    public class Articulo
+    {
+        public Guid ArticuloId { get; set; }
+        public string? Titular { get; set; }
+        public string? Subtitulo { get; set; }
+        public string? Cuerpo { get; set; }
+    }
+}
